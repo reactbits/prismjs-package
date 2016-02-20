945627107c7d887923dd4ed951a29cc33cde5661
sergeyt <stodyshev@gmail.com>
2016-02-20 18:49:34 +0600

    updated prism

--- src/prism.js
+++ src/prism.js
@@ -95,19 +95,19 @@ var _ = _self.Prism = {
 		insertBefore: function (inside, before, insert, root) {
 			root = root || _.languages;
 			var grammar = root[inside];
-			
+
 			if (arguments.length == 2) {
 				insert = arguments[1];
-				
+
 				for (var newToken in insert) {
 					if (insert.hasOwnProperty(newToken)) {
 						grammar[newToken] = insert[newToken];
 					}
 				}
-				
+
 				return grammar;
 			}
-			
+
 			var ret = {};
 
 			for (var token in grammar) {
@@ -127,7 +127,7 @@ var _ = _self.Prism = {
 					ret[token] = grammar[token];
 				}
 			}
-			
+
 			// Update references in other language definitions
 			_.languages.DFS(_.languages, function(key, value) {
 				if (value === root[inside] && key != inside) {
@@ -158,12 +158,19 @@ var _ = _self.Prism = {
 		}
 	},
 	plugins: {},
-	
+
 	highlightAll: function(async, callback) {
-		var elements = document.querySelectorAll('code[class*="language-"], [class*="language-"] code, code[class*="lang-"], [class*="lang-"] code');
+		var env = {
+			callback: callback,
+			selector: 'code[class*="language-"], [class*="language-"] code, code[class*="lang-"], [class*="lang-"] code'
+		};
+
+		_.hooks.run("before-highlightall", env);
+
+		var elements = env.elements || document.querySelectorAll(env.selector);
 
 		for (var i=0, element; element = elements[i++];) {
-			_.highlightElement(element, async === true, callback);
+			_.highlightElement(element, async === true, env.callback);
 		}
 	},
 
@@ -4758,6 +4765,10 @@ Prism.hooks.add('wrap', function(env) {
 			if (delimiter === '$') {
 				pattern = /([^\$])(\$(\{.*?\}|[\w\.]+))/;
 			}
+
+			// To prevent double HTML-ecoding we have to decode env.content first
+			env.content = env.content.replace(/&amp;/g, '&').replace(/&lt;/g, '<');
+
 			env.content = Prism.highlight(env.content, {
 				'expression': {
 					pattern: pattern,
